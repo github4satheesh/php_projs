b4a345641414e606a8cf807988d4fcac5eda9c66
Satheesh Chelladurai <146996740+github4satheesh@users.noreply.github.com>
2024-10-07 20:06:25 -0400

    Update index.php
    query updated

--- index.php
+++ index.php
@@ -12,14 +12,6 @@
 		$password = '@dminpwd123';
 		$db_name = 'mysql';
 
-		// Create database connection
-		//$conn = new mysqli($servername, $username, $password, $dbname);
-
-		// Check connection
-		if ($conn->connect_error) {
-			die("Connection failed: " . $conn->connect_error);
-		}
-
 //Establishes the connection
 $conn = mysqli_init();
 mysqli_real_connect($conn, $host, $username, $password, $db_name, 3306);
@@ -27,9 +19,20 @@ if (mysqli_connect_errno($conn)) {
 die('Failed to connect to MySQL: '.mysqli_connect_error());
 }
 
+//Create an Insert prepared statement and run it
+$product_name = 'BrandNewProduct';
+$product_color = 'Blue';
+$product_price = 15.5;
+if ($stmt = mysqli_prepare($conn, "INSERT INTO Products (ProductName, Color, Price) VALUES (?, ?, ?)")) {
+mysqli_stmt_bind_param($stmt, 'ssd', $product_name, $product_color, $product_price);
+mysqli_stmt_execute($stmt);
+printf("Insert: Affected %d rows\n", mysqli_stmt_affected_rows($stmt));
+mysqli_stmt_close($stmt);
+}
+
 //Run the Select query
 printf("Reading data from table: \n");
-$res = mysqli_query($conn, 'SELECT * FROM Employee');
+$res = mysqli_query($conn, 'SELECT * FROM Products');
 while ($row = mysqli_fetch_assoc($res)) {
 var_dump($row);
 }
