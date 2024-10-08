080739ab7be562d151f628b7356fbfa5b128ca43
Satheesh Chelladurai <146996740+github4satheesh@users.noreply.github.com>
2024-10-07 20:10:25 -0400

    Update index.php
    product query

--- index.php
+++ index.php
@@ -4,13 +4,12 @@
 	<title>MySQL Table Viewer - Title</title>
 </head>
 <body>
-	<h1>MySQL Table Viewer - Satheesh New</h1>
-	<?php
-		// Define database connection variables
-		$host = 'csksqlserver1.mysql.database.azure.com';
-		$username = 'cskadmin';
-		$password = '@dminpwd123';
-		$db_name = 'mysql';
+	<h1>MySQL Table Viewer Products - Satheesh New</h1>
+<?php
+$host = 'csksqlserver1.mysql.database.azure.com';
+$username = 'cskadmin';
+$password = '@dminpwd123';
+$db_name = 'mysql';
 
 //Establishes the connection
 $conn = mysqli_init();
@@ -19,27 +18,19 @@ if (mysqli_connect_errno($conn)) {
 die('Failed to connect to MySQL: '.mysqli_connect_error());
 }
 
-//Create an Insert prepared statement and run it
+//Run the Update statement
 $product_name = 'BrandNewProduct';
-$product_color = 'Blue';
-$product_price = 15.5;
-if ($stmt = mysqli_prepare($conn, "INSERT INTO Products (ProductName, Color, Price) VALUES (?, ?, ?)")) {
-mysqli_stmt_bind_param($stmt, 'ssd', $product_name, $product_color, $product_price);
+$new_product_price = 15.1;
+if ($stmt = mysqli_prepare($conn, "UPDATE Products SET Price = ? WHERE ProductName = ?")) {
+mysqli_stmt_bind_param($stmt, 'ds', $new_product_price, $product_name);
 mysqli_stmt_execute($stmt);
-printf("Insert: Affected %d rows\n", mysqli_stmt_affected_rows($stmt));
+printf("Update: Affected %d rows\n", mysqli_stmt_affected_rows($stmt));
+
+//Close the connection
 mysqli_stmt_close($stmt);
 }
 
-//Run the Select query
-printf("Reading data from table: \n");
-$res = mysqli_query($conn, 'SELECT * FROM Products');
-while ($row = mysqli_fetch_assoc($res)) {
-var_dump($row);
-}
-
-//Close the connection
 mysqli_close($conn);
-
-	?>
+?>
 </body>
 </html>
